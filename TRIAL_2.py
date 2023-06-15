6a3c561cee6e288453b5f6056fbd39a0a4aca1b9
StevenGbena <136390353+StevenGbena@users.noreply.github.com>
2023-06-15 22:06:12 +0100

    Add files via upload

---
+++ TRIAL_2.py
@@ -0,0 +1,12 @@
+for i in range(3):
+    username=input('enter username')
+    password=input('enter password')
+    if username=='stephen' and password=='rose':
+            print('succesful login')
+            break
+    else:
+            print('wrong credentials')
+            
+            
+else:
+        print('account is locked')                
