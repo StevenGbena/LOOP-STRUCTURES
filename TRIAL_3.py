d8d9d4063fa1f49503746595b91aef4606f45f89
StevenGbena <136390353+StevenGbena@users.noreply.github.com>
2023-06-17 15:45:27 +0100

    Add files via upload

--- TRIAL_3.py
+++ TRIAL_3.py
@@ -1,9 +1,11 @@
-count=0
+user=[3,5,-4,2,-4,-5-3,9,10,-45]
+sum=0
 
-while count<20:
-        count+=1
-        user1=int(input('enter the number'))
-        if user1<0:
-         continue
-        print(user1)
-      
+for i in user:
+     if i <0:
+          continue
+     else:
+       sum=sum+i
+       print(sum)   
+              
+    
